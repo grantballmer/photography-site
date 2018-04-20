eb191184bd52feccdd5afe6c17d11fe175619679
Grant <grant.a.ballmer@gmail.com>
2018-04-20 20:52:47 +0900

    More tiles

---
+++ app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('.hero').slick({
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    pauseOnHover: false,
+    adaptiveHeight: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    arrows: false
+  });
+})
